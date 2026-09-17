f194d3060928b049d7383fa0eb4023e8f3728eb3
Prof-RafaGrillo <125394387+Prof-RafaGrillo@users.noreply.github.com>
2026-09-17 09:15:24 -0300

    .

--- js/main.js
+++ js/main.js
@@ -44,10 +44,6 @@ function mostraResultado(){
     caixaAlternativas.textContent ="";
 }
 
-function aleatorio(lista){
-    const posicao = Math.floor(Math.random()*lista.length)
-    console.log(posicao)
-    return(lista[posicao])
-}
+
 
 mostraPergunta()
